eb4691549388789f67f174b19b681faef73f84e8
Steven? <linguinealfredo@gmail.com>
2017-10-14 15:21:20 -0700

    Add hello world code

--- HelloSwift/main.swift
+++ HelloSwift/main.swift
@@ -1,12 +1,1 @@
-//
-//  main.swift
-//  HelloSwift
-//
-//  Created by UCode on 10/14/17.
-//  Copyright © 2017 The Illuminati. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+print("Hello world!")
