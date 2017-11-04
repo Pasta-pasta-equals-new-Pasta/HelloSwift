b92512f6eb09a530381d9ec7870420f94ef86c37
your name <linguinealfredo@gmail.com>
2017-11-04 16:11:10 -0700

    the message

--- HelloSwift/main.swift
+++ HelloSwift/main.swift
@@ -1,1 +1,68 @@
-print("Hello world!")
+import Foundation
+
+
+//print("Enter your fahrenheit.")
+//let f = Double(readLine()!)
+//
+//    if f != nil {
+//print((5.0/9.0)*(f! - 32.0))
+//}
+//    else {
+//        print("Put a number")
+//}
+
+func cToF(c: Double) -> Double {
+    return (9.0/5.0) * c + 32.0
+}
+
+func fToC(f: Double) -> Double {
+    return (5.0/9.0) * (f - 32.0)
+}
+
+func cToK(c: Double) -> Double {
+    return c + 273.15
+}
+func kToC(k: Double) -> Double {
+    return k - 273.15
+}
+
+func fToK(f: Double) -> Double {
+    return cToK(c:fToC(f: f))
+}
+
+func kToF(k: Double) -> Double {
+    return cToF(c:kToC(k: k))
+}
+
+print("Choose your fighter")
+let x = readLine()
+
+if let _temp = x {
+    
+    let temp = _temp.components(separatedBy: " ")
+    
+    if temp[1] == "K" {
+        print("\(kToC(k: Double(temp[0])!)) C")
+        print("\(kToF(k: Double(temp[0])!)) F")
+    }
+    
+    else if temp[1] == "F" {
+        print( "\(fToC(f: Double(temp[0])!)) C")
+        print( "\(fToK(f: Double(temp[0])!)) K")
+    }
+    
+    else if temp[1] == "C" {
+        print( "\(cToF(c: Double(temp[0])!)) F")
+        print( "\(cToK(c: Double(temp[0])!)) K")
+    }
+    
+    else {
+        print("Please enter a valid temperature unit")
+    }
+    
+
+    
+}
+
+
+//gulp
