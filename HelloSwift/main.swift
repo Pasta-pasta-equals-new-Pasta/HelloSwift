d2b0eed8933f048454bbcc9cf0428f450e26dc66
the ting go skrra pa pa ka ka ka skidi ki pa pa and a tu tu ku ku boom <linguinealfredo@gmail.com>
2017-11-05 15:53:31 -0800

    jjgjos

--- HelloSwift/main.swift
+++ HelloSwift/main.swift
@@ -34,35 +34,109 @@ func kToF(k: Double) -> Double {
     return cToF(c:kToC(k: k))
 }
 
-print("Choose your fighter")
-let x = readLine()
+//print("Choose your fighter")
+//let x = readLine()
+//
+//if let _temp = x {
+//    
+//    let temp = _temp.components(separatedBy: " ")
+//    
+//    if temp[1] == "K" {
+//        print("\(kToC(k: Double(temp[0])!)) C")
+//        print("\(kToF(k: Double(temp[0])!)) F")
+//    }
+//    
+//    else if temp[1] == "F" {
+//        print( "\(fToC(f: Double(temp[0])!)) C")
+//        print( "\(fToK(f: Double(temp[0])!)) K")
+//    }
+//    
+//    else if temp[1] == "C" {
+//        print( "\(cToF(c: Double(temp[0])!)) F")
+//        print( "\(cToK(c: Double(temp[0])!)) K")
+//    }
+//    
+//    else {
+//        print("Please enter a valid temperature unit")
+//    }
+//    
+//
+//    
+//}
 
-if let _temp = x {
-    
-    let temp = _temp.components(separatedBy: " ")
-    
-    if temp[1] == "K" {
-        print("\(kToC(k: Double(temp[0])!)) C")
-        print("\(kToF(k: Double(temp[0])!)) F")
-    }
-    
-    else if temp[1] == "F" {
-        print( "\(fToC(f: Double(temp[0])!)) C")
-        print( "\(fToK(f: Double(temp[0])!)) K")
-    }
-    
-    else if temp[1] == "C" {
-        print( "\(cToF(c: Double(temp[0])!)) F")
-        print( "\(cToK(c: Double(temp[0])!)) K")
-    }
-    
-    else {
-        print("Please enter a valid temperature unit")
-    }
-    
+var score           = 0
+let questions       = [
+    (
+        value:    50,
+        question: "Which of the following programming languages is OOP?",
+        answers: [
+            (
+                correct: false,
+                answer: "C"
+            ),
+            (
+                correct: false,
+                answer: "SQL"
+            ),
+            (
+                correct: true,
+                answer: "C#"
+            ),
+            (
+                correct: false,
+                answer: "Haskell"
+            )
+        ]
+    ),
+    (
+        value:    50,
+        question: "Which of the following is a C# list method?",
+        answers: [
+            (
+                correct: true,
+                answer: ".Contains"
+            ),
+            (
+                correct: false,
+                answer: ".Sort"
+            ),
+            (
+                correct: false,
+                answer: ".Copy"
+            ),
+            (
+                correct: false,
+                answer: ".Reverse"
+            )
+        ]
+    )
+]
 
+
+
+for question in questions {
+    
+    print(question.question)
+    
+    for (index, answer) in question.answers.enumerated() {
+        print("  \(index + 1): \(answer.answer)")
+    }
+    
+    let rawResponse = readLine()
+  
+    if let response = rawResponse {
+        
+        let responseIndex = Int(response)! - 1
+        
+        if question.answers[responseIndex].correct {
+            score += question.value
+        }
+        
+    }
+    
     
 }
 
+print(score)
 
 //gulp
